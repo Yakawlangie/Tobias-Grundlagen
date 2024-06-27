ed00e7973a0624226435ada16c98b8b16e0dee7f
Yakawlangie <javadsaber@gmail.com>
2024-06-27 08:46:57 +0200

    Projektdateien hinzufügen.

---
+++ Concepts/Program.cs
@@ -0,0 +1,143 @@
+﻿string name = "tobi";
+/*
+ PascalCase => WorttrennungenWerdenGrossGeschrieben
+  camelCase => worttrennungenWerdenKleingeschrieben
+  snake_case => worttrennungen_werden_kleingeschrieben
+  kebab-case => worttrennungen-werden-kleingeschrieben
+ */
+
+/*
+ DateTime ist einer von vielen komplexen Datentypen in C#.
+  DateTime ist ein Strukturtyp, der Datum und Uhrzeit darstellt.
+  Komplexe DatenTypen sind Objekte, die aus mehreren Werten bestehen, welche
+    ebenfalls komplex oder auch primitv sein können.
+   Objekte können nur in der Konsole ausgegeben werden, wenn sie eine Methode
+    ToString() haben.
+ */
+DateTime date = DateTime.Now;
+Console.WriteLine(date.Year);
+
+Console.WriteLine(name);
+
+
+bool is_true = false;
+// Funktionen
+// x = 5y
+
+string? test_null = null;
+Console.WriteLine("buxtehude");
+//
+// var y = Console.ReadLine();
+
+var trimmed_name = name.Trim();
+
+//Console.WriteLine(x);
+
+var test = 5.5;
+
+// implicit conversion
+Console.WriteLine((int)test + (int)test);
+
+// explicit conversion
+// int num = int.Parse(test);
+
+void PrintName()
+{
+  Console.WriteLine("Hello World!");
+}
+/*
+ * JavaScript
+ function printName() {
+  console.log("Hello World!");
+ }
+ * Python
+ def printName():
+   print("Hello World!")
+ # hier gehts dann weiter
+ */
+
+PrintName();
+PrintName();
+
+string GetGreeting()
+{
+  return "Herzlich Willkommen!";
+}
+
+var greeting =  GetGreeting();
+
+Console.WriteLine(greeting);
+
+// Chararrays
+var my_array = "Feierabend!";
+var my_letter = my_array[0];
+
+// Collections-Basics
+// Array
+string[] namen = new string[5];
+string[] alternative_namen = ["Tobi", "Popstar", "Lehrer"]; // max 3 items
+string[] alternative_namen1 = { "Ab", "Bc", "Cd" };
+namen[0] = "Tobi";
+namen[1] = "Melanie";
+namen[2] = "Torben";
+// namen[99999999999999998] = "Buxtehude!";
+Console.WriteLine(namen[0][0]);
+
+int increment_me = 0;
+// Die folgenden 2 Zeilen sind syntaktisch gleich
+increment_me = increment_me + 1;
+increment_me += 1;
+// increment-operator
+increment_me++;
+Console.WriteLine("Increment me is: ");
+Console.WriteLine(increment_me);
+// String-Interpolation
+// Hat einige Nachteile, was Sicherheit angeht (s. Vertiefung)
+Console.WriteLine($"Increment me is: {increment_me}");
+
+
+// Array ist eine Liste an Werten
+// Eine kontinuirliche Speicherregion, für mehrere Daten eines gewissen Types
+// Ein Array hat immer eine feste Größe, welche bei der Initialisierung festgelegt werden muss
+// Array.Length ist immer die maximale Länge des Arrays
+int[] nums = new int[5];
+nums[0] = 10;
+/*
+ For Loop - 4 Komponenten
+   1. Deklaration der Zählervariablen
+   2. Bedingung der Schleife (Wann zu Ende?)
+   3. Operation, welche nach jeder Iteration ausgeführt wird
+      -> idr. Inkrement (i++) oder Dekrement (i--)
+   4. Codeblock, welcher definiert, was bei jeder iteration der Schleife passieren soll.
+ */
+for (int i = 0; i < nums.Length; i++)
+{
+  Console.WriteLine(nums[i]);
+}
+
+int test_me = 0;
+
+// Variablen, welche innerhalb eines Blocks deklariert sind,
+//   können nicht außerhalb dieses Blocks verwendet werden.
+{
+  Console.WriteLine(test_me);
+  int my_cool_variable = 0;
+  Console.WriteLine(my_cool_variable);
+  { 
+    Console.WriteLine(my_cool_variable);
+  }
+}
+Console.WriteLine(my_cool_variable);
+
+
+/*
+{
+  var myvar1 = 0;
+}
+{
+  var myvar1 = 10;
+}
+var myvar2 = 10;
+var myvar2 = 20;
+Console.WriteLine(myvar1);
+*/
---
+++ TD/Program.cs
@@ -0,0 +1,99 @@
+﻿/* Program-Einstiegspunkt C#
+ * namespace TD
+{
+  // Programmeinstiegspunkt (Wo das Programm anfängt auszuführen.)
+  // => Program.Main
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello, World!");
+      Console.ReadLine();
+      // hier...
+      var x = Start();
+      Console.WriteLine(x);
+    }
+    static string Start()
+    {
+      Console.WriteLine("Buxtehude!");
+      return "blubb";
+    }
+    // const login = function() {...}
+    // login()
+  }
+}
+*/
+
+// Menü anzeigen mit Optionen
+Console.WriteLine("Willkommen bei TD!");
+
+string[] todos = new string[20];
+
+// das hier ist aktuell noch null!!!
+// Console.WriteLine(todos[9]);
+
+do
+{
+  Console.Clear();
+  Console.WriteLine("Was möchtest du tun?");
+  Console.WriteLine("\t1. Neues Todo erstellen\n\t2. Todos anzeigen\n\t3. Todo abhacken\n\t4. Todo überschreiben");
+  var input = Console.ReadLine();
+
+  switch (input)
+  {
+    case "1":
+      Console.WriteLine("Was hast du zu tun? <Enter zum bestätigen>");
+      var todo = Console.ReadLine();
+      // Iterieren durch alle Todos (auch die leeren)
+      for (int i = 0; i < todos.Length; i++)
+      {
+        // Wenn das aktuelle Element im Array leer ist, dann...
+        if (todos[i] == null)
+        {
+          // Fügen wir das Todo hinzu
+          todos[i] = todo;
+          Console.WriteLine("Todo hinzugefügt: " + todos[i]);
+          break;
+        }
+      }
+      break;
+    case "2":
+      Console.WriteLine("Du hast folgende Aufgaben:");
+      for (int i = 0; i < todos.Length; i++)
+      {
+        Console.WriteLine($"\t{i}. {todos[i]}");
+        // if (todos[i] != null)
+      }
+      Console.WriteLine("Drücke <ENTER> um zurück ins Menü zu kommen.");
+      Console.ReadKey();
+      break;
+    case "3":
+      // Löschen
+      Console.WriteLine("Welches Todo möchtest du löschen?");
+      for (int i = 0; i < todos.Length; i++)
+      {
+        Console.WriteLine($"\t{i}. {todos[i]}");
+      }
+      var todoIndexString = Console.ReadLine();
+      int todoIndex = int.Parse(todoIndexString);
+      // float todoIndex = float.Parse(todoIndexString);
+      // int todoIndex1 = Convert.ToInt32(todoIndexString);
+      todos[todoIndex] = null;
+      break;
+    case "4":
+      // Updaten/Verändern eines Todos (verändern des strings)
+      Console.WriteLine("Welches Todo möchtest du anpassen?");
+      for (int i = 0; i < todos.Length; i++)
+      {
+        Console.WriteLine($"\t{i}. {todos[i]}");
+      }
+      var indexString = Console.ReadLine();
+      int index = int.Parse(indexString);
+
+      Console.WriteLine("Was möchtest du stattdessen tun?");
+      var newTodo = Console.ReadLine();
+      todos[index] = newTodo;
+      break;
+  }
+} while (true);
+
